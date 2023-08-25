a9997fe14b1031c0944ceb1507d638bc3b3ed780
RicardoOliveros <rica.oli0811@gmail.com>
2023-08-24 19:22:45 -0500

    24/08/2023 7:22

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
 AOS.init();
+const url = 'https://rickandmortyapi.com/api/character';
 let boton = document.getElementById("mode");
 let before = document.getElementById("beforeBtn");
 let after = document.getElementById("afterBtn");
+const Main = document.getElementById('root');
 let NumberPage = 1;
+
+
+// DARK MODE
 
 boton.addEventListener('click', () => {
     document.body.classList.toggle("dark-mode")
 })
 
 
-
-
-const Main = document.getElementById('root');
+// PAGINACION
 
 function GetInfo(NumberPage) {
     const Url = `https://rickandmortyapi.com/api/character?page=${NumberPage}`;
@@ -32,10 +35,10 @@ function GetInfo(NumberPage) {
                 Main.innerHTML += `
         <div>
             <div class="containerCard">
-                <div class="titulosCard"> 
 
-                
+                <div class="titulosCard">                 
                 <h2>${nombre}</h2>
+
                 <img src="${item.image}">
                 </div>
 
@@ -48,20 +51,57 @@ function GetInfo(NumberPage) {
         </div>
         `
             })
-
-
         })
-
 }
 
 GetInfo(NumberPage)
 
-btnScrollToTop.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
+
+// BUSCADOR
+
+const buscar = document.getElementById("btnSearch");
+const inputB = document.getElementById('searchCharac');
+
+buscar.addEventListener('click', () => {
+
+    fetch(url)
+
+        .then(response => {
+            return response.json()
+        })
+
+        .then(data => {
+            const Info = data.results;
+            const word = inputB.value.toLowerCase();
+            console.log(word);
+            const array = Info.filter(item => item.name.toLowerCase().includes(word));
+            Main.innerHTML = "";
+
+            array.map(item => {
+                const nombre = item.name.length > 13 ? item.name.slice(0, 11) + '..' : item.name;
+                Main.innerHTML += `
+        <div>
+            <div class="containerCard">
+
+                <div class="titulosCard">                 
+                <h2>${nombre}</h2>
+
+                <img src="${item.image}">
+                </div>
+
+                <div class="informationCards">
+                    <p>Status: ${item.status}</p>
+                    <p>Species: ${item.species}</p>
+                    <p>Gender: ${item.gender}</p>
+                </div>
+            </div>
+        </div>
+        `
+            })
+        })
 })
+
+// BOTONES PARA PASAR DE PAGINA
 
 before.addEventListener("click", () => {
     NumberPage--;
